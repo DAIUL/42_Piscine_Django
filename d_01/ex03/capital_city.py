97d7e6288e6ccf653a6d8ac7324ea1e99d54b439
DAIUL <daiul@Akukordinateur.local>
2025-03-06 19:43:36 +0900

    d01 ex02 ex03

---
+++ d_01/ex03/capital_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+
+def	giveCapital():
+
+	if len(sys.argv) != 2:
+		sys.exit(0)
+
+	states = {
+		"Oregon" : "OR",
+		"Alabama" : "AL", "New Jersey": "NJ",
+		"Colorado" : "CO"
+	}
+
+	capital_cities = {
+		"OR": "Salem",
+		"AL": "Montgomery",
+		"NJ": "Trenton",
+		"CO": "Denver"
+	}
+
+	state_name = states.get(sys.argv[1])
+
+	print(capital_cities.get(state_name, "Unknown state"))
+
+if __name__ == '__main__':
+	giveCapital()
