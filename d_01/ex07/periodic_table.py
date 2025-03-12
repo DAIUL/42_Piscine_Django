274a87e0fa0943bd4c26877e58f02732580a95c4
DAIUL <daiul@Akukordinateur.local>
2025-03-12 11:57:52 +0900

    d01 ex07 wip-2

--- d_01/ex07/periodic_table.py
+++ d_01/ex07/periodic_table.py
@@ -12,26 +12,59 @@ def periodicTab():
 
 			element_values = {}
 			element_name, properties = line.split(" = ")
+			element_values["name"] = element_name
 			pairs = properties.split(", ")
 
 			for pair in pairs:
 				key, value = pair.split(":")
 				element_values[key.strip()] = value.strip()
 
-			element = {element_name: element_values}
-			periodic_tab.append(element)
+			periodic_tab.append(element_values)
 
 	return periodic_tab
+
+def elementGenerator(element: dict) -> str:
+
+	elements_html = f"""		<td> 
+		<h4>{element['name']}</h4> 
+		<ul> 
+			<li>{element['number']}</li>
+			<li>{element['small']}</li>
+			<li>{element['molar']}</li>
+		</ul>
+	</td>"""
+	
+	return elements_html
 
 def htmlGenerator():
 
 	periodic_tab = periodicTab()
 
+	html_elements = []
+
+	for element in periodic_tab:
+		html_elements.append(elementGenerator(element))
+
+	table_body = "\n".join(html_elements)
+
 	html_content = f"""
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Periodic Table</title>
+	</head>
+	<body>
+		<table>
+		{table_body}
+		</table>
+	</body>
+	</html>
 	"""
 
 	with open("periodic_table.html", "w", encoding="utf-8") as file:
 		file.write(html_content)
 
 if __name__ == '__main__':
-	periodicTab()
+	htmlGenerator()
