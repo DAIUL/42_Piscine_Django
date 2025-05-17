4bfa818184afc75901b35519e86b190044894a07
DAIUL <quentin.puigsp@gmail.com>
2025-05-17 18:37:43 +0900

    d_01 ex07

--- d_01/ex07/periodic_table.py
+++ d_01/ex07/periodic_table.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def periodicTab():
 
 	periodic_tab = []
@@ -36,16 +35,52 @@ def elementGenerator(element: dict) -> str:
 	
 	return elements_html
 
+def orderTab(periodic_tab: dict):
+
+	empty_tab = [["" for _ in range(18)] for _ in range(7)]
+
+	line = 0
+
+	for element in periodic_tab:
+		empty_tab[line][int(element["position"])] = elementGenerator(element)
+		if int(element["position"]) == 17:
+			line += 1
+
+	return empty_tab
+		
+
 def htmlGenerator():
 
 	periodic_tab = periodicTab()
 
-	html_elements = []
+	html_elements = orderTab(periodic_tab)
 
-	for element in periodic_tab:
-		html_elements.append(elementGenerator(element))
+	table_body = ""
 
-	table_body = "\n".join(html_elements)
+	for row in html_elements:
+		table_body += "<tr>\n"
+		for cell in row:
+			if cell:
+				table_body += f"{cell}\n"
+			else:
+				table_body += "\t<td></td>\n"
+		table_body += "</tr>\n"
+
+	css_content = f"""
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+	* {{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+	font-family: Roboto, sans-serif;
+	}}
+
+	td {{
+	border: 1px solid black;
+	padding: 10px;
+	}}
+	"""
 
 	html_content = f"""
 	<!DOCTYPE html>
@@ -53,6 +88,7 @@ def htmlGenerator():
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="periodic_table.css">
 		<title>Periodic Table</title>
 	</head>
 	<body>
@@ -65,6 +101,9 @@ def htmlGenerator():
 
 	with open("periodic_table.html", "w", encoding="utf-8") as file:
 		file.write(html_content)
+
+	with open("periodic_table.css", "w", encoding="utf-8") as file:
+		file.write(css_content)
 
 if __name__ == '__main__':
 	htmlGenerator()
